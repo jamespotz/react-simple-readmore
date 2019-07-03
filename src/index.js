91e13e3406e5dca87747f4cc0e540b8920ccada2
James Robert Rooke <thepoltergeist23@gmail.com>
2019-07-03 16:17:44 +0800

    add git repo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,120 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState, useEffect, useRef } from 'react';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const getDefaultStyle = (duration, easing) => {
+	return {
+		overflow: 'hidden',
+		transition: `height ${duration}ms ${easing}`
+	};
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const ReadMore = ({
+	minHeight,
+	displayHeight,
+	label,
+	labelShown,
+	timing,
+	timingFunction,
+	defaultShownOnLess,
+	...rest
+}) => {
+	const container = useRef();
+	const _container = useRef();
+	const [height, setHeight] = useState(50);
+	const [beforeHeight, setBeforeHeight] = useState(50);
+	const [finalHeight, setFinalHeight] = useState('auto');
+	const [show, setShown] = useState(false);
+	const [duration, setDuration] = useState(350);
+	const [easing, setEasing] = useState('ease-in');
+	const [btnLabel, setBtnLabel] = useState('Read More');
+	const [btnLabelShown, setBtnLabelShown] = useState('Read Less');
+
+	useEffect(() => {
+		if (minHeight) setHeight(minHeight);
+		if (displayHeight) setFinalHeight(displayHeight);
+		if (label) setBtnLabel(label);
+		if (labelShown) setBtnLabelShown(labelShown);
+		if (timing) setDuration(timing);
+		if (timingFunction) setEasing(timingFunction);
+	}, [displayHeight, label, labelShown, minHeight, timing, timingFunction]);
+
+	const getContainerHeight = () => {
+		const target = _container.current;
+		return `${target.scrollHeight}px`;
+	};
+
+	const showContents = () => {
+		const newHeight = getContainerHeight();
+		setBeforeHeight(height);
+		setHeight(newHeight);
+
+		if (defaultShownOnLess) {
+			setShown(true);
+			setTimeout(() => {
+				setHeight(finalHeight);
+			}, duration);
+			return;
+		}
+
+		setTimeout(() => {
+			setHeight(finalHeight);
+			setShown(true);
+		}, duration);
+	};
+
+	const hideContents = () => {
+		const { current } = container;
+		setHeight(current.scrollHeight);
+
+		setTimeout(() => {
+			setHeight(beforeHeight);
+		}, 1);
+
+		setTimeout(() => {
+			setShown(false);
+		}, duration);
+	};
+
+	const toggleHeight = () => {
+		if (show) {
+			hideContents();
+			return;
+		}
+
+		showContents();
+	};
+
+	const hideBtn = () => {
+		if (!_container.current) return false;
+		console.log(_container.current.scrollHeight);
+		return _container.current.scrollHeight <= height;
+	};
+
+	const showChildren = () => {
+		if (defaultShownOnLess && !show) return defaultShownOnLess;
+		return rest.children;
+	};
+
+	return (
+		<div>
+			<div
+				ref={container}
+				style={{ height, ...getDefaultStyle(duration, easing) }}
+			>
+				{showChildren()}
+			</div>
+			{hideBtn() ? null : (
+				<button onClick={toggleHeight} className={rest.btnClassName}>
+					{show ? btnLabelShown : btnLabel}
+				</button>
+			)}
+			<div
+				style={{ position: 'absolute', top: '-9999px', left: '-9999px' }}
+				ref={_container}
+			>
+				{rest.children}
+			</div>
+		</div>
+	);
+};
+
+export default ReadMore;
